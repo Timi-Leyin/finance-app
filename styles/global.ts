6006f5898cdb607eae55117f77d67d96df2a9e82
Original Timi <timileyinoyelekan11@gmail.com>
2023-12-10 13:16:11 +0100

    feat: global style

---
+++ styles/global.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const globalStyle = StyleSheet.create({
+    link:{
+        color:"blue",
+        textDecorationLine:"underline",
+        fontWeight:"600"
+    }
+})
+
+export default globalStyle
