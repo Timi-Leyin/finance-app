af54545d2fca3ace3cec80c05d4d35cdf16c65e2
Original Timi <timileyinoyelekan11@gmail.com>
2023-12-18 15:11:38 +0100

    setup route

--- app/RootApp.tsx
+++ app/RootApp.tsx
@@ -1,19 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
-import OnBoarding1 from '../pages/OnBoarding1';
-
+import OnBoarding1 from "../pages/OnBoarding1";
+import OnBoarding2 from "../pages/OnBoarding2";
+import GetStarted from "../pages/GetStarted";
 
 const RootApp = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen options={{title: 'Welcome'}} name="Home" component={OnBoarding1} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
-}
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Onboarding-1"
+      >
+        <Stack.Screen name="Onboarding-1" component={OnBoarding1} />
+        <Stack.Screen name="Onboarding-2" component={OnBoarding2} />
+        <Stack.Screen name="Get Started" component={GetStarted} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default RootApp
+export default RootApp;
