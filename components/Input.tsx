5a019acc3f084f03a72dfaae645b7be32befe727
Original Timi <timileyinoyelekan11@gmail.com>
2024-01-01 22:28:18 +0100

    switched from class Component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,39 +1,39 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { Component } from 'react'
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { Component } from "react";
 
-export class Input extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={style.label}>Label</Text>
-          <TextInput style={style.input} placeholder="Name" id="name"  />
-        <Text style={style.error}>Invalid Inout</Text>
-      </View>
-    )
-  }
-}
-
+const Input = ({label, error, placeholder, id, type}:{label:string, error?:string, placeholder?:string, id?:string, type?:any}) => {
+  return (
+    <View style={style.container}>
+      {/* <Text style={style.label}>{label}</Text> */}
+      <TextInput style={style.input} keyboardType={type} placeholder={placeholder} id={id} />
+      <Text style={style.error}>{error || ""}</Text>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({
-    input:{
-        padding:20,
-        backgroundColor:"#ddd",
-        borderRadius:20,
-        fontSize:20,
-        fontFamily:"molgen",
-        marginHorizontal:30
+    container:{
+        paddingVertical:4,
     },
-    label:{
-        marginHorizontal:35,
-        paddingVertical:10,
-        fontSize:30,
-        fontFamily:"molgen"
-    },
-    error:{
-        color:"#ff0000",
-        padding:5,
-        marginHorizontal:35
-    }
-})
+  input: {
+    padding: 20,
+    backgroundColor: "#eee",
+    borderRadius: 20,
+    fontSize: 18,
+    fontFamily: "Titillium_Regular",
+    marginHorizontal: 20,
+  },
+  label: {
+    marginHorizontal: 30,
+    paddingBottom: 4,
+    fontSize: 22,
+    fontFamily: "Titillium_Bold",
+  },
+  error: {
+    color: "#ff0000",
+    padding: 5,
+    marginHorizontal: 35,
+  },
+});
 
-export default Input
+export default Input;
